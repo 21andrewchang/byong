b66427f0a2157e787badee7edc2621077e8052fd
Andrew Chang <21andrewch@gmail.com>
2024-03-04 18:55:01 -0600

    feat: linking works

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,31 @@
 <script>
 	import arrow from '$lib/images/Arrow.png';
+	import { page } from '$app/stores';
+
+	// const scrollThreshold = 100;
+	//
+	// function handleNavigation() {
+	// 	if (window.scrollY > scrollThreshold) {
+	// 		console.log('navigating to home');
+	// 	}
+	// }
+	//
+	// onMount(() => {
+	// 	window.addEventListener('scroll', handleNavigation);
+	//
+	// 	return () => {
+	// 		window.removeEventListener('scroll', handleNavigation);
+	// 	};
+	// });
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>Landing</title>
 	<meta name="description" content="Landing Page" />
 </svelte:head>
-<body>
-	<div class="container">
-		<h1>byong</h1>
+<body class="landing">
+	<div class="link">
+		<a href="/home" aria-current={$page.url.pathname === '/'}> byong </a>
 	</div>
 	<div class="image">
 		<img src={arrow} alt="arrow" class="arrow" />
@@ -16,31 +33,38 @@
 </body>
 
 <style>
-	:global(body) {
-		background-color: black;
+	.landing {
 		background-image: url('$lib/images/byongbg.png');
 		background-size: cover;
-	}
-	.image img {
-		margin: auto;
-	}
-	.image {
+		background-position: center;
 		display: flex;
-		justify-self: center;
+		justify-content: center; /* Horizontally center content */
+		align-items: center; /* Vertically center content */
+		height: 100vh; /* Set the height to fill the viewport */
 	}
 
 	.container {
-		display: flex;
-		height: 90vh;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
+		text-align: center; /* Center text within container */
 	}
 
-	h1 {
+	.link {
+		margin-bottom: 20px; /* Add some space between the link and the image */
+	}
+
+	.image {
+		display: flex; /* Ensure the image is centered properly */
+		justify-content: center; /* Center the image horizontally */
+	}
+
+	.image img {
+		max-width: 100%; /* Ensure the image doesn't exceed container width */
+	}
+
+	a {
 		color: #fff;
 		font-size: 50px;
 		font-weight: normal; /* Adjust font weight as needed */
+		text-decoration: none;
 	}
 
 	@font-face {
@@ -53,5 +77,6 @@
 	body {
 		font-family: 'Marcellus', sans-serif;
 		overflow: hidden;
+		background-image: transparent; /* Ensure body has no background */
 	}
 </style>
---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Home Page" />
+</svelte:head>
+<body class="home">
+	<div>
+		<h1>byong</h1>
+	</div>
+</body>
+
+<style>
+	.home {
+		background-color: black;
+	}
+</style>
