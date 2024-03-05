9818d11cb495ae5e2294f9be674b51b9794ddb06
Andrew Chang <21andrewch@gmail.com>
2024-03-05 05:03:34 -0600

    feat: button link

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -10,28 +10,40 @@
 </svelte:head>
 
 <body class="home-page">
+	<div class="title">byong</div>
 	<div class="grid-container">
 		<div class="container">
-			<a class="text" href="/street" aria-current={$page.url.pathname === '/'}>street</a>
-			<div id="street" class="button"></div>
+			<a href="/street" aria-current={$page.url.pathname === '/'} class="button-link">
+				<div id="street" class="button"></div>
+				<span class="text">street</span>
+			</a>
 		</div>
 		<div class="container">
-			<a class="text" href="/architecture" aria-current={$page.url.pathname === '/'}>architecture</a
-			>
-			<div id="architecture" class="button"></div>
+			<a href="/architecture" aria-current={$page.url.pathname === '/'} class="button-link">
+				<div id="architecture" class="button"></div>
+				<span class="text">architecture</span>
+			</a>
 		</div>
 		<div class="container">
-			<a class="text" href="/portrait" aria-current={$page.url.pathname === '/'}>portrait</a>
-			<div id="portrait" class="button"></div>
+			<a href="/portrait" aria-current={$page.url.pathname === '/'} class="button-link">
+				<div id="portrait" class="button"></div>
+				<span class="text">portrait</span>
+			</a>
 		</div>
 		<div class="container">
-			<a class="text" href="/bnw" aria-current={$page.url.pathname === '/'}>bnw</a>
-			<div id="bnw" class="button"></div>
+			<a href="/bnw" aria-current={$page.url.pathname === '/'} class="button-link">
+				<div id="bnw" class="button"></div>
+				<span class="text">bnw</span>
+			</a>
 		</div>
 	</div>
 </body>
 
 <style>
+	.title {
+		color: white;
+		padding-bottom: 5px;
+	}
 	.container {
 		display: flex;
 		flex-direction: column;
@@ -46,9 +58,10 @@
 		z-index: 1;
 		position: relative;
 		color: white;
-		font-size: 20px;
+		font-size: 10px;
 		text-decoration: none;
 		opacity: 0;
+		font-weight: lighter;
 	}
 	.container:hover .text {
 		opacity: 1;
@@ -58,6 +71,7 @@
 		width: 400px;
 		height: 270px;
 		position: absolute;
+		text-decoration: none;
 		top: 0;
 		left: 0;
 		background-repeat: no-repeat;
@@ -68,6 +82,19 @@
 
 	.container:hover .button {
 		filter: brightness(20%);
+		text-decoration: none;
+	}
+	.button-link {
+		display: inline-block;
+		text-decoration: none;
+	}
+
+	.button-link:hover .button {
+		filter: brightness(20%);
+	}
+
+	.button-link:hover .text {
+		opacity: 1;
 	}
 
 	#architecture {
@@ -89,7 +116,7 @@
 	@font-face {
 		font-family: 'Marcellus';
 		font-style: normal;
-		font-weight: normal;
+		font-weight: 200;
 		src: url('../../lib/fonts/Marcellus-Regular.ttf') format('truetype');
 	}
 
@@ -97,10 +124,11 @@
 		font-family: 'Marcellus', sans-serif;
 		display: flex;
 		background-color: black;
-		align-items: center;
+		align-items: center; /* Align items vertically */
 		overflow: hidden;
 		justify-content: center;
 		height: 100vh;
+		flex-direction: column;
 	}
 
 	.grid-container {
