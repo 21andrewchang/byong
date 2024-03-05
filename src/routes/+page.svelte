58f8fa0d560bc2c73ccd97a6664fad214a432e2d
Andrew Chang <21andrewch@gmail.com>
2024-03-05 04:50:59 -0600

    feat: buttons work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,72 +1,24 @@
+<!-- LandingPage.svelte -->
 <script>
-	import arrow from '$lib/images/Arrow.png';
+	export let pageTitle = 'Landing Page';
 	import { page } from '$app/stores';
-
-	// const scrollThreshold = 100;
-	//
-	// function handleNavigation() {
-	// 	if (window.scrollY > scrollThreshold) {
-	// 		console.log('navigating to home');
-	// 	}
-	// }
-	//
-	// onMount(() => {
-	// 	window.addEventListener('scroll', handleNavigation);
-	//
-	// 	return () => {
-	// 		window.removeEventListener('scroll', handleNavigation);
-	// 	};
-	// });
 </script>
 
 <svelte:head>
-	<title>Landing</title>
+	<title>{pageTitle}</title>
 	<meta name="description" content="Landing Page" />
 </svelte:head>
+
 <body class="landing">
-	<div class="link">
-		<a href="/home" aria-current={$page.url.pathname === '/'}> byong </a>
-	</div>
-	<div class="image">
-		<img src={arrow} alt="arrow" class="arrow" />
-	</div>
+	<a href="/home" aria-current={$page.url.pathname === '/'}> byong </a>
 </body>
 
 <style>
-	.landing {
-		background-image: url('$lib/images/byongbg.png');
-		background-size: cover;
-		background-position: center;
-		display: flex;
-		justify-content: center; /* Horizontally center content */
-		align-items: center; /* Vertically center content */
-		height: 100vh; /* Set the height to fill the viewport */
-	}
-
-	.container {
-		text-align: center; /* Center text within container */
-	}
-
-	.link {
-		margin-bottom: 20px; /* Add some space between the link and the image */
-	}
-
-	.image {
-		display: flex; /* Ensure the image is centered properly */
-		justify-content: center; /* Center the image horizontally */
-	}
-
-	.image img {
-		max-width: 100%; /* Ensure the image doesn't exceed container width */
-	}
-
 	a {
-		color: #fff;
-		font-size: 50px;
-		font-weight: normal; /* Adjust font weight as needed */
+		color: white; /* Set link color to white */
+		font-size: 45px; /* Adjust font size */
 		text-decoration: none;
 	}
-
 	@font-face {
 		font-family: 'Marcellus';
 		font-style: normal;
@@ -76,7 +28,18 @@
 
 	body {
 		font-family: 'Marcellus', sans-serif;
+		background-color: black;
+	}
+	.landing {
+		background-image: url('../lib/images/byongbg.png');
+		background-repeat: no-repeat;
+		background-attachment: fixed;
+		background-position: center;
+		background-size: cover;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
 		overflow: hidden;
-		background-image: transparent; /* Ensure body has no background */
 	}
 </style>
--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,18 +1,111 @@
+<!-- HomePage.svelte -->
 <script>
+	export let pageTitle = 'Home Page';
+	import { page } from '$app/stores';
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>{pageTitle}</title>
 	<meta name="description" content="Home Page" />
 </svelte:head>
-<body class="home">
-	<div>
-		<h1>byong</h1>
+
+<body class="home-page">
+	<div class="grid-container">
+		<div class="container">
+			<a class="text" href="/street" aria-current={$page.url.pathname === '/'}>street</a>
+			<div id="street" class="button"></div>
+		</div>
+		<div class="container">
+			<a class="text" href="/architecture" aria-current={$page.url.pathname === '/'}>architecture</a
+			>
+			<div id="architecture" class="button"></div>
+		</div>
+		<div class="container">
+			<a class="text" href="/portrait" aria-current={$page.url.pathname === '/'}>portrait</a>
+			<div id="portrait" class="button"></div>
+		</div>
+		<div class="container">
+			<a class="text" href="/bnw" aria-current={$page.url.pathname === '/'}>bnw</a>
+			<div id="bnw" class="button"></div>
+		</div>
 	</div>
 </body>
 
 <style>
-	.home {
+	.container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		position: relative;
+		width: 400px;
+		height: 270px;
+	}
+
+	.text {
+		z-index: 1;
+		position: relative;
+		color: white;
+		font-size: 20px;
+		text-decoration: none;
+		opacity: 0;
+	}
+	.container:hover .text {
+		opacity: 1;
+	}
+
+	.button {
+		width: 400px;
+		height: 270px;
+		position: absolute;
+		top: 0;
+		left: 0;
+		background-repeat: no-repeat;
+		background-position: center;
+		background-size: cover;
+		transition: filter 0.3s ease;
+	}
+
+	.container:hover .button {
+		filter: brightness(20%);
+	}
+
+	#architecture {
+		background-image: url('../../lib/images/architecture.jpg');
+	}
+
+	#street {
+		background-image: url('../../lib/images/street.jpg');
+	}
+
+	#bnw {
+		background-image: url('../../lib/images/bnw.jpg');
+	}
+
+	#portrait {
+		background-image: url('../../lib/images/portrait.jpg');
+	}
+
+	@font-face {
+		font-family: 'Marcellus';
+		font-style: normal;
+		font-weight: normal;
+		src: url('../../lib/fonts/Marcellus-Regular.ttf') format('truetype');
+	}
+
+	.home-page {
+		font-family: 'Marcellus', sans-serif;
+		display: flex;
 		background-color: black;
+		align-items: center;
+		overflow: hidden;
+		justify-content: center;
+		height: 100vh;
+	}
+
+	.grid-container {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		gap: 5px;
 	}
 </style>
